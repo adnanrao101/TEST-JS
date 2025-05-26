cb7e1fcffd761813cd5248d8f79b2b7a73d71647
Adnan Rao <adnanejaz2874@gmail.com>
2025-05-26 16:42:56 +0500

    Q5 Complete

---
+++ script5.js
@@ -0,0 +1,13 @@
+let a = 82
+
+if (++a === 83){
+    alert("given condition for varibale is true")
+}else{
+    alert("given condition for varibale is false")
+}
+
+// if ("car" > "cat"){
+//     alert("smaller")
+// }else{
+//     alert("larger")
+// }
