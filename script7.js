83a4fdf169c9f60f6c5e18c8ed29d9f03123071b
Adnan Rao <adnanejaz2874@gmail.com>
2025-05-26 17:27:48 +0500

    Q7 Complete

---
+++ script7.js
@@ -0,0 +1,7 @@
+const secret = prompt("GUESS THE SECRET NUMBER IN 1 TO 10")
+
+if (secret == 7){
+    document.write("BINGO YOU ARE CORRECT SECRET NUMBER IS 7 :)")
+}else[
+    document.write("Oops!... THAT'S NOT A SECRET NUMBER")
+]
