08d2d997c457574e398d512ad0b981a238a16e8c
Adnan Rao <adnanejaz2874@gmail.com>
2025-05-26 15:28:23 +0500

    QUESTION 2 DONE

---
+++ script2.js
@@ -0,0 +1,9 @@
+const gender = prompt("Are You Male Or Female ?").toLowerCase();
+
+if (gender == "male"){
+    document.write("GOOD MORNING SIR :)")
+}else{
+    document.write("GOOD MORNING MA'AM :)")
+    
+}
+
