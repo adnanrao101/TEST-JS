2668dd4b99379e07ebdc6d306372fce9b19b8416
Adnan Rao <adnanejaz2874@gmail.com>
2025-05-26 15:51:30 +0500

    q2 change

--- script2.js
+++ script2.js
@@ -1,22 +1,22 @@
-// const gender = prompt("Are You Male Or Female ?").toLowerCase();
+const gender = prompt("Are You Male Or Female ?").toLowerCase();
 
-// if (gender == "male"){
-//     document.write("GOOD MORNING SIR :)")
-// }else{
-//     document.write("GOOD MORNING MA'AM :)")
-    
-// }
+if (gender === "male"){
+    document.write("GOOD MORNING SIR :)")
+} else if (gender === "female") {
+    document.write("GOOD MORNING MA'AM :)")
+}else{
+    document.write("Please select a valid gender.")
+}
 
+// function greet() {
+//       const gender = document.getElementById("gender").value.toLowerCase();
+//       const greeting = document.getElementById("greeting");
 
-function greet() {
-      const gender = document.getElementById("gender").value.toLowerCase();
-      const greeting = document.getElementById("greeting");
-
-      if (gender === "male") {
-        greeting.textContent = "GOOD MORNING SIR :)";
-      } else if (gender === "female") {
-        greeting.textContent = "GOOD MORNING MA'AM :)";
-      } else {
-        greeting.textContent = "Please select a valid gender.";
-      }
-    }
+//       if (gender === "male") {
+//         greeting.textContent = "GOOD MORNING SIR :)";
+//       } else if (gender === "female") {
+//         greeting.textContent = "GOOD MORNING MA'AM :)";
+//       } else {
+//         greeting.textContent = "Please select a valid gender.";
+//       }
+//     }
---
+++ script3.js
@@ -0,0 +1,3 @@
+const signal = prompt("what is traffic signal color ?")
+
+if (signal === "")
